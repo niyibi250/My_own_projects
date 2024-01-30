a225e4c6b5aeecbaa1e8f180708f6d95b3c2825c
niyibi250 <ericniyibizi1998@gmail.com>
2024-01-30 16:05:43 +0200

    new change

---
+++ momgoose_project/return_all_collection_in_mongodb.js
@@ -0,0 +1,29 @@
+let {MongoClient} = require('mongodb')
+
+let client = new MongoClient('mongodb://localhost:27017/')
+let name_of_database = 'ericfirstdatabase'
+let name_of_collection = 'products'
+
+async function startdatabaseconnection()
+{
+  try{
+    await client.connect()
+    let database=client.db(name_of_database)
+    let result =await database.listCollections().toArray()
+   result.forEach(function(collect,index)
+   {
+    console.log(collect.name)
+   })
+  }
+  catch(error){
+    console.log('samething went wrong')
+  }
+  finally{
+    await client.close()
+    console.log('the connection has to be closed')
+  }
+}
+
+startdatabaseconnection()
+
+
